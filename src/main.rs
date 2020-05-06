98969d5ca4883d565c1f085f3a140b8fdc904f6a
ssempervirens <camillefassett@gmail.com>
2020-05-06 12:58:42 -0700

    add anyhow and update rand, fix empty vector bug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rand::Rng;
 
+use anyhow::Error;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use std::fs::File;
 use std::io::prelude::*;
-use anyhow::Error;
-
+use anyhow::anyhow;
 
 fn main() {
     let secret_word = select_word().unwrap();
@@ -14,14 +16,19 @@ fn select_word() -> Result<String, Error> {
     // open and read word options file, select a word and return it as string
     let mut f = File::open("word-options.txt")?;
     let mut word_blob = String::new();
-        f.read_to_string(& mut word_blob)?;
+    f.read_to_string(&mut word_blob)?;
 
     let words: Vec<&str> = word_blob.trim().split(',').collect();
 
     // pick one of the words by random index
-    let rand_index = rand::thread_rng().gen_range(0, words.len());
+    // let rand_index = rand::thread_rng().gen_range(0, words.len());
 
-    Ok(words[rand_index].into())
+    // Ok(words[rand_index].into())
+    Ok(words
+        .choose(&mut thread_rng())
+        .ok_or_else(|| anyhow!("Word vector is empty"))?
+        .to_string()
+        )
 }
 
 // need to take user input to guess a character
@@ -29,4 +36,3 @@ fn select_word() -> Result<String, Error> {
 // if turns equals 0, print you lost sry, bad man live
 // failed counter for getting a character wrong. if failed == 0, print you may have won the game,
 // but is the death penalty really a win?
-
